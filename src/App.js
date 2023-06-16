808b78b243a42037ac57eb207ab5b84d26c0e903
mukhlisrambe5 <mukhlis.rambe5@gmail.com>
2023-06-16 16:14:15 +0700

    add footer menu

--- src/App.js
+++ src/App.js
@@ -22,9 +22,18 @@ const App = () => {
                 </Routes>
             </div>
         </Layout>
-       </div>
-       <div className='footer'>
-        
+      
+       <div className='footer' >
+        <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
+          CryptoVerse<br/>
+          All rights reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+          </div>
        </div>
     </div>
 
